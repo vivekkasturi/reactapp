eb0197199761d7de36dfc719cb6fe16786d96646
vivekkasturi <kasturi.vivek994@gmail.com>
2024-01-24 20:15:32 +0530

    Create and Delete functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  
+ const [inputdata, setInputdata]=useState("");
+ const [arraydata, setArraydata] = useState([]);
+
+  const AddData = (e) => {
+  
+   setInputdata(e.target.value)
+  };
+
+  const Submitdata = ()=>{
+
+   setArraydata(()=>[...arraydata,inputdata]) ///spread the data and push
+ 
+    return console.log(arraydata);
+  };
+
+ const deletedata=(data)=>{
+
+  console.log(data);
+
+    const deleteddata= arraydata.filter((element)=>{if(element!=data){
+      return true
+    }})
+    console.log("deleteddata:-",deleteddata);
+    setArraydata(()=>[...deleteddata]);
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To do Application</h1>
+        <hr />
+        <input type="text" name="inputbox" placeholder="Add item..."  onChange={AddData}/>
+        <br />
+        <button onClick={Submitdata}>Add</button>
+    {inputdata}
+    {
+ 
+      arraydata.map((data,index)=>{
+
+        return(
+          
+          <div>
+          
+            <ul> 
+          <li key={index}>{data} <button onClick={()=>deletedata(data,index)}>Delete</button></li>
+            </ul>
+
+        
+
+            </div>
+        )
+      })
+    }
     </div>
   );
 }
