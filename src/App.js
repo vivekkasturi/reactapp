fa44f9b80165272fa2c8233f1a87b79258ee6e6c
vivekkasturi <kasturi.vivek994@gmail.com>
2024-01-25 17:44:47 +0530

    Added Edit and update functionality

--- src/App.js
+++ src/App.js
@@ -5,40 +5,62 @@ function App() {
   
  const [inputdata, setInputdata]=useState("");
  const [arraydata, setArraydata] = useState([]);
+const [indexvalue,setIndexvalue] = useState(null);
+const [isVisible,setIsVisible] = useState(false);
 
   const AddData = (e) => {
   
    setInputdata(e.target.value)
+
+  
   };
 
   const Submitdata = ()=>{
 
    setArraydata(()=>[...arraydata,inputdata]) ///spread the data and push
- 
+ setInputdata("")
     return console.log(arraydata);
   };
 
- const deletedata=(data)=>{
+ const deletedata=(data,index)=>{
 
   console.log(data);
 
-    const deleteddata= arraydata.filter((element)=>{if(element!=data){
+    const deleteddata= arraydata.filter((element,index1)=>{ if(index1!=index){
       return true
     }})
     console.log("deleteddata:-",deleteddata);
     setArraydata(()=>[...deleteddata]);
  }
 
+const editfn=(data,index)=>{
+
+  setInputdata(data);
+  setIndexvalue(index);
+setIsVisible(true);
+
+}
+
+const updatedata =()=>{
+
+  arraydata[indexvalue]=inputdata;
+  console.log(arraydata[indexvalue]);
+  setArraydata(()=>[...arraydata])
+  setInputdata("");
+  setIsVisible(false);
+}
   return (
     <div className="App">
       <h1>To do Application</h1>
         <hr />
-        <input type="text" name="inputbox" placeholder="Add item..."  onChange={AddData}/>
+        <input type="text" name="inputbox" placeholder="Add item..." value={inputdata} onChange={AddData}/>
         <br />
-        <button onClick={Submitdata}>Add</button>
-    {inputdata}
+       
+       { isVisible ? " ":  <button onClick={Submitdata}>Add</button>}
+       {isVisible ? <button onClick={updatedata}>Update</button>: " "} 
     {
  
+
       arraydata.map((data,index)=>{
 
         return(
@@ -46,11 +68,9 @@ function App() {
           <div>
           
             <ul> 
-          <li key={index}>{data} <button onClick={()=>deletedata(data,index)}>Delete</button></li>
+          <li key={index}>{data} <button onClick={()=>deletedata(data,index)}>Delete</button> <button onClick={()=>editfn(data,index)}>edit</button> </li>
+
             </ul>
-
-        
-
             </div>
         )
       })
